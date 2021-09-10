857fcc478924decb596f3f93e2585900bd68dda1
Aiman Imtiaz <aiman.imtiaz@u.yale-nus.edu.sg>
2021-09-10 12:41:33 +0800

    empty plugin

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for plugin
+
+## Unreleased changes
---
+++ out/production/plugin/Main.hs
@@ -0,0 +1,12 @@
+module DoNothing.Plugin (plugin) where
+import GhcPlugins
+
+plugin :: Plugin
+plugin = defaultPlugin {
+  installCoreToDos = install
+  }
+
+install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
+install _ todo = do
+  putMsgS "Hello!"
+  return todo
