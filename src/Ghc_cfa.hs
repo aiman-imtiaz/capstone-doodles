82bb619c983ec7e102338cd8f19ba0f6d3126eb6
Aiman Imtiaz <aiman.imtiaz@u.yale-nus.edu.sg>
2021-09-20 18:45:34 +0800

    print dots for every expr

--- src/Ghc_cfa.hs
+++ src/Ghc_cfa.hs
@@ -6,17 +6,69 @@ plugin = defaultPlugin {
   installCoreToDos = install
   }
 
-plugin_pass :: [ModGuts] -> CoreM [ModGuts]
-plugin_pass modGuts = do
+
+pluginPass :: ModGuts -> CoreM ModGuts
+pluginPass modGuts = do
   dyn <- getDynFlags
-  putMsgS (showPpr dyn (mg_binds modGuts))
+  let binds = mg_binds modGuts
+  putMsgS (showPpr dyn binds)
+  printDots binds
   putMsgS "Hello! from plugin pass"
   return modGuts
+  
+printDotBindExpr :: CoreExpr -> CoreM ()
+printDotBindExpr (Var id) = do
+  putMsgS "."
+  return ()
+printDotBindExpr (Lit literal) = do
+  putMsgS "."
+  return ()
+printDotBindExpr (App exprb argb) = do
+  putMsgS "."
+  return ()
+printDotBindExpr (Lam b exprB) = do
+  putMsgS "."
+  return ()
+printDotBindExpr (Let bindB exprB) = do
+  putMsgS "."
+  return ()
+printDotBindExpr (Case exprB b t altB) = do
+  putMsgS "."
+  return ()
+printDotBindExpr (Cast exprB coercion) = do
+  putMsgS "."
+  return ()
+printDotBindExpr (Tick tickishId exprB) = do
+  putMsgS "."
+  return ()
+printDotBindExpr (Type t) = do
+  putMsgS "."
+  return ()
+printDotBindExpr (Coercion coercion) = do
+  putMsgS "."
+  return ()
+  
+printDotsBind :: CoreBind -> CoreM ()
+printDotsBind (NonRec b e) = do
+  printDotBindExpr e
+  putMsgS "."
+  return ()
+printDotsBind (Rec bList) = do
+  putMsgS "."
+  return ()
+
+printDots :: [CoreBind] -> CoreM ()
+printDots (b : t) = do
+  printDotsBind b
+  printDots t
+printDots [] = do
+  return ()
+  
 
 install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
 install _ todo = do
   dyn <- getDynFlags
   putMsgS (showPpr dyn todo)
   putMsgS "Hello!"
-  let plugin_todo = CoreDoPluginPass "ghc_cfa" plugin_pass
+  let plugin_todo = CoreDoPluginPass "ghc_cfa" pluginPass
   return (plugin_todo : todo)
